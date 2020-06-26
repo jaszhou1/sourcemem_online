805c8e9176098ea86ff56fb98d385a6c46395274
Jason Zhou <jasonz1@student.unimelb.edu.au>
2020-06-27 01:49:41 +1000

    Add word lists by session
    Added two different word lists created by shuffling and splitting the original word list in half. The intention is to use these for two experimental sessions across which no word is repeated. Updated htmls have been added as templates (s1 and s2), but are not currently called in the online version of the experiment.

--- experiment/stimuli/csv_to_json.R
+++ experiment/stimuli/csv_to_json.R
@@ -1,6 +1,6 @@
 library(jsonlite)
 setwd("~/GitHub/sourcemem_online/experiment/stimuli")
 
-wordlist <- read.csv('wordlist.csv', fileEncoding="UTF-8-BOM")
+wordlist <- read.csv('word_list_freq6_manual.csv', fileEncoding="UTF-8-BOM")
 words <- as.character(wordlist$word)
 jsonwords <- toJSON(words, pretty = TRUE)
