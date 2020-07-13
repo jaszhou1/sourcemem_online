a4c977ed1d294ea81d13c31dce0f3e41d41c00ec
Jason Zhou <jasonz1@student.unimelb.edu.au>
2020-07-13 16:36:24 +1000

    Sort dataset
    Added function to sort through the raw data loaded into R from the server database and extract recognition trial data. Extraction of source information required additional data (stimulus word) to be added to the recall data structure.

--- analysis/sourcemem-online.R
+++ analysis/sourcemem-online.R
@@ -22,14 +22,43 @@ completed.users <- get.completed.users(SERVER.BASE.URL, SERVER.PORT,
                                        SERVER.MASTER.API.KEY)
 
 
-this.user.data <- get.last.experiment.data.by.user.id(SERVER.BASE.URL, completed.users[[1]],
+this.user.data <- get.last.experiment.data.by.user.id(SERVER.BASE.URL, completed.users[[8]],
                                                       SERVER.PORT, SERVER.MASTER.API.KEY)
 
 ## Extract the required information for each stimuli across the trial types.
-data <- data.frame(matrix(ncol=9,nrow=length(this.user.data$present_trials), dimnames=list(NULL, c("word", "stimulus", "contition",
+data <- data.frame(matrix(ncol=8,nrow=length(this.user.data$present_trials), dimnames=list(NULL, c("word", "is_sequential",
                                                                "recog_rating","recog_RT","target_angle",
-                                                               "response_angle","response_error","response_RT"))))
+                                                               "response_angle","response_error","source_RT"))))
 
-for(i in length(this.user.data$present_trials)){
-  data$word[i] <- this.user.data$present_trials[[i]]$target_word
-}
+## Extract presentation data
+  for(i in 1:length(this.user.data$present_trials)){
+    data$word[i] <- this.user.data$present_trials[[i]]$target_word
+    data$is_sequential[i] <- this.user.data$present_trials[[i]]$presentation_sequential
+  }
+
+## Function to sort through the scrambled blocks and find the index needed for extraction
+  find_index <- function(x, list){
+    for (i in 1:length(list)){
+      if (list[[i]]$stimulus == x){
+        return(i)
+      } 
+    }
+  }
+
+## Extract recognition data
+  words <- unique(data$word)
+
+  for (i in 1:length(words)){
+    index <- find_index(words[i],this.user.data$confidence_trials)
+    data$recog_rating[i] <- this.user.data$confidence_trials[[index]]$response
+    data$recog_RT[i] <- this.user.data$confidence_trials[[index]]$rt
+  } 
+  
+## Extract source recall data
+  for (i in 1:length(words)){
+    index <- find_index(words[i],this.user.data$recall_trials)
+    data$response_angle[i] <- this.user.data$recall_trials[[index]]$hitting_angle
+    data$source_RT[i] <- this.user.data$recall_trials[[index]]$response_time
+  } 
+  
+  
