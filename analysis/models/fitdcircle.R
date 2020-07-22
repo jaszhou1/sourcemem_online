7fedf73b02253c4572444696bc6f2b00e1706eaf
Jason Zhou <jasonz1@student.unimelb.edu.au>
2020-07-22 16:02:59 +1000

    Add MATLAB circular diffusion model
    Added barebones version of the matlab code used to fit the circular diffusion model to data from previous experiments. This will be used as a base from which the circular diffusion model will be ported into R.

---
+++ analysis/models/fitdcircle.R
@@ -0,0 +1,7 @@
+## fitdcircle.R
+#
+## Adaptation of Philip Smith's circular diffusion code originally written in MATLAB.
+## Assumes the eta components in the x and y directions are the same.
+#
+## Jason Zhou <jasonz1 AT student DOT unimelb DOT edu DOT au>
+
