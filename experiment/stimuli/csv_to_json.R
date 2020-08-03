f391b659c42a5b0b395f9e63fb71bb4cb6866a2e
Jason Zhou <jasonz1@student.unimelb.edu.au>
2020-08-03 14:59:48 +1000

    Add Stimuli for Three Sessions
    Widened the stimuli criteria from SUBTLEX word frequency of 1-6 to 0.75-7. This gives just enough words, after manual filtering out of inappropriate words, to have three sessions of 240 trials each (120 stimuli and 120 foils).

--- experiment/stimuli/csv_to_json.R
+++ experiment/stimuli/csv_to_json.R
@@ -1,9 +1,23 @@
 library(jsonlite)
 setwd("~/GitHub/sourcemem_online/experiment/stimuli")
 
-wordlist <- read.csv('word_list_freq6_manual.csv', fileEncoding="UTF-8-BOM")
+wordlist <- read.csv('wordlist_freq7.csv', fileEncoding="UTF-8-BOM")
 
 # Randomise order (for session split)
 set.seed(42)
 words <- as.character(wordlist[sample(nrow(wordlist)),])
-jsonwords <- toJSON(words, pretty = TRUE)
+
+# Number of sessions to split stimuli into
+num_sessions <- 3
+
+# Function to split character array into sessions
+chunk <- function(x,n) split(x, cut(seq_along(x), n, labels = FALSE))
+sessions <- chunk(words,num_sessions)
+
+#jsonwords <- toJSON(words, pretty = TRUE)
+
+json_s1 <- toJSON(sessions[1], pretty = TRUE)
+json_s2 <- toJSON(sessions[2], pretty = TRUE)
+json_s3 <- toJSON(sessions[3], pretty = TRUE)
+
+
