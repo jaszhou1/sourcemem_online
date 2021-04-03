d4f44c8d13e75bee68c7c8e4bb1e1f73529bdef7
Jason Zhou <jasonz1@student.unimelb.edu.au>
2021-04-03 15:57:04 +1100

    Add intrusion offsets to csv
    When processing data, add all the intrusions for a trial to the row for that trial.

--- analysis/sourcemem-online-bays.R
+++ analysis/sourcemem-online-bays.R
@@ -21,6 +21,13 @@ library(ggplot2)
 source("access-data.R")
 sessions <- c(1,2,3)
 
+# Function to compute angular difference
+
+angle_diff <- function(a,b){
+  diff <- atan2(sin(a-b), cos(a-b))
+  return(diff)
+}
+
 ## Get the started users
 started.users <- get.started.users(SERVER.BASE.URL, SERVER.PORT,
                                    SERVER.MASTER.API.KEY)
@@ -35,15 +42,17 @@ completed.users <- get.completed.users(SERVER.BASE.URL, SERVER.PORT,
 # this.user.info <- get.user.information(SERVER.BASE.URL, started.users[[1]],
 #                                        SERVER.PORT, SERVER.MASTER.API.KEY)
 
+
 get_session <- function(p,s){
+  
   this.session.data <- get.session.data.by.user.id(SERVER.BASE.URL, p, s,
                                                    SERVER.PORT, SERVER.MASTER.API.KEY)
-  
   ## Extract the required information for each stimuli across the trial types.
   data <- data.frame(matrix(ncol=10,nrow=length(this.session.data$present_trials), dimnames=list(NULL, c("word", "is_sequential",
-                                                                                                        "recog_rating","recog_RT","target_angle",
-                                                                                                        "response_angle","response_error","source_RT", "valid_RT",
-                                                                                                        "block"))))
+                                                                                                         "recog_rating","recog_RT","target_angle",
+                                                                                                         "response_angle","response_error","source_RT", "valid_RT",
+                                                                                                         "block"))))
+  ## Add a number of empty columns for intrusions that is equivalent to the number of trials per block -1 
   
   ## Extract presentation data
   for(i in 1:length(this.session.data$present_trials)){
@@ -96,10 +105,41 @@ get_session <- function(p,s){
   }
   
   data$recog_band <- ifelse(data$recog_rating >= 1 & data$recog_rating <= 3, 'Unrecognized',
-                            ifelse(data$recog_rating >=8 & data$recog_rating <9, 'Low',
+                            ifelse(data$recog_rating >=8 & data$recog_rating <=9, 'Low',
                                    ifelse(data$recog_rating ==0, 'High','N/A')))
   
-  #data <- data[data$valid_RT == TRUE,]
+  # Assemble a list of intrusions for each trial, and append this to the data structure
+  num_intrusions <- 9 # This is n-1 where n is number of trials per block
+  
+  intrusion_labels <- vector()
+  for (i in 1:num_intrusions){
+    intrusion_labels[i] <- sprintf('intrusion_%i',i)
+  }
+  
+  intrusion_offset_labels <- vector()
+  for (i in 1:num_intrusions){
+    intrusion_offset_labels[i] <- sprintf('intrusion_offset%i',i)
+  }
+  
+  intrusions <- data.frame(matrix(ncol=9,nrow=125))
+  colnames(intrusions) <- intrusion_labels
+  
+  intrusion_offsets <- data.frame(matrix(ncol=9,nrow=125))
+  colnames(intrusion_offsets) <- intrusion_offset_labels
+  
+  for (i in 1:nrow(data)){
+    this_block_data <- data[data$block == data$block[i],]
+    # The possible intrusions are items in the same block as this trial, but do not have the same stimulus word
+    this_block_intrusions <- this_block_data[this_block_data$word != data$word[i],]$target_angle
+    this_intrusion_offsets <- angle_diff(data$target_angle[i], this_block_intrusions)
+    if (data$block[i] == -1){
+      this_block_intrusions <- c(this_block_intrusions, NA, NA, NA, NA, NA)
+      this_intrusion_offsets <- c(this_intrusion_offsets, NA, NA, NA, NA, NA) # Filling up with NAs for practice block
+    }
+    intrusions[i,] <- this_block_intrusions
+    intrusion_offsets[i,] <- this_intrusion_offsets
+  }
+  data <- cbind(data,intrusions, intrusion_offsets)
   return(data)
 }
 
@@ -199,106 +239,49 @@ plot_participant <- function(p){
   
   filename = sprintf('Subject_%.0f_Error.png', p)
   ggsave(filename, plot = error)
-
+  
   filename = sprintf('Subject_%.0f_RT.png', p)
   ggsave(filename, plot = rt)
-
+  
   return(this.user.data)
 }
 
 ## Save all the data as one csv
 
-all.data = data.frame()
-# Participant Loop
-for(h in 1:length(completed.users)){
-  this.user.data = data.frame()
-  # Session Loop
-  for(j in 1:length(sessions)){
-
-    this.session.data <- get.session.data.by.user.id(SERVER.BASE.URL, completed.users[[h]], j,
-                                                     SERVER.PORT, SERVER.MASTER.API.KEY)
-
-    ## Extract the required information for each stimuli across the trial types.
-    data <- data.frame(matrix(ncol=11,nrow=length(this.session.data$present_trials), dimnames=list(NULL, c("word", "is_sequential",
-                                                                                                          "recog_rating","recog_RT","target_angle",
-                                                                                                          "response_angle","response_error","source_RT", "valid_RT",
-                                                                                                          "block","session"))))
-    ## Save session number
-    data$session <- j
-    
-    ## Extract presentation data
-    for(i in 1:length(this.session.data$present_trials)){
-      data$word[i] <- this.session.data$present_trials[[i]]$target_word
-      data$is_sequential[i] <- this.session.data$present_trials[[i]]$presentation_sequential
+save.all.data <- function(){
+  all.data = data.frame()
+  # Participant Loop
+  for(h in 1:length(completed.users)){
+    this.user.data = data.frame()
+    # Session Loop
+    for(j in 1:length(sessions)){
+      data <- get_session(completed.users[[h]],j)
+      # Bind sessions together
+      
+      this.user.data <- rbind(this.user.data, data)
+      # Filter out invalid RTs
+      #this.user.data <- this.user.data[this.user.data$valid_RT == TRUE,]
+      
     }
-
-    ## Function to sort through the scrambled blocks and find the index needed for extraction
-    find_recog_index <- function(x, list){
-      for (i in 1:length(list)){
-        if (list[[i]]$stimulus == x){
-          return(i)
-        }
-      }
-    }
-
-    ## The stimulus word is called "target_angle" for the recall trials. Could change how it is named in the database,
-    ## but easier for now to just use the different name here
-    find_source_index <- function(x, list){
-      for (i in 1:length(list)){
-        if (list[[i]]$target_word == x){
-          return(i)
-        }
-      }
-    }
-
-    ## Extract recognition data
-    words <- unique(data$word)
-
-    for (i in 1:length(words)){
-      index <- find_recog_index(words[i],this.session.data$confidence_trials)
-      data$recog_rating[i] <- this.session.data$confidence_trials[[index]]$response
-      data$recog_RT[i] <- this.session.data$confidence_trials[[index]]$rt
-    }
-
-    ## Extract source recall data
-    for (i in 1:length(words)){
-      index <- find_source_index(words[i],this.session.data$recall_trials)
-      data$block[i] <- this.session.data$recall_trials[[index]]$block
-      data$target_angle[i] <- this.session.data$recall_trials[[index]]$target_angle
-      data$response_angle[i] <- this.session.data$recall_trials[[index]]$hitting_angle
-      data$response_error[i] <- this.session.data$recall_trials[[index]]$angular_error
-      ## Correcting for an error in the old javascript calculation of angular error
-      if(data$response_error[i] < -pi){
-        data$response_error[i] <- data$response_error[i] + 4*pi
-      }
-      data$source_RT[i] <- this.session.data$recall_trials[[index]]$response_time
-      data$valid_RT [i] <- (this.session.data$recall_trials[[index]]$num_fast_attempts == 0 &&
-                              this.session.data$recall_trials[[index]]$num_slow_attempts == 0)
-    }
-
-    # Bind sessions together
-
-    this.user.data <- rbind(this.user.data, data)
-    # Filter out invalid RTs
-    #this.user.data <- this.user.data[this.user.data$valid_RT == TRUE,]
-
+    this.user.data$participant <- h
+    all.data <- rbind(all.data, this.user.data)
   }
- this.user.data$participant <- h
- all.data <- rbind(all.data, this.user.data)
+  
+  # Recode the confidence ratings to be between 1 and 6
+  all.data[all.data$recog_rating == 8,]$recog_rating <- 4
+  all.data[all.data$recog_rating == 9,]$recog_rating <- 5
+  all.data[all.data$recog_rating == 0,]$recog_rating <- 6
+  
+  # Save data as .csv file
+  write.csv(all.data,"~/GitHub/sourcemem_online/analysis/sourcemem_data_all_intrusions.csv", row.names = FALSE)
+  
+  ## Save all users data
+  for(i in 1:length(completed.users)){
+    this.user.data <- get.last.experiment.data.by.user.id(SERVER.BASE.URL, completed.users[[i]],
+                                                          SERVER.PORT, SERVER.MASTER.API.KEY)
+    filename = sprintf('Subject%s.RData', i)
+    save(this.user.data, file = filename)
+  }
 }
 
-# Recode the confidence ratings to be between 1 and 6
-all.data[all.data$recog_rating == 8,]$recog_rating <- 4
-all.data[all.data$recog_rating == 9,]$recog_rating <- 5
-all.data[all.data$recog_rating == 0,]$recog_rating <- 6
 
-# Save data as .csv file
-write.csv(all.data,"~/GitHub/sourcemem_online/analysis/sourcemem_data_bays1.csv", row.names = FALSE)
-
-## Save all users data
-for(i in 1:length(completed.users)){
-  this.user.data <- get.last.experiment.data.by.user.id(SERVER.BASE.URL, completed.users[[i]],
-                                                        SERVER.PORT, SERVER.MASTER.API.KEY)
-  filename = sprintf('Subject%s.RData', i)
-  save(this.user.data, file = filename)
-}
