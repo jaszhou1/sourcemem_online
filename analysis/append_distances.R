8bc16984f9289ce4bffb0bf6c56fc5e4d9846868
jason zhou <45774327+jaszhou1@users.noreply.github.com>
2021-09-09 12:25:23 +1000

    Tidy up script that pulls data from datastore
    This script cuts out the preliminary plotting functions included in earlier versions, and also appends the spatial, orthographic, and semantic distance values directly into the .csv file output at the end.

---
+++ analysis/append_distances.R
@@ -0,0 +1,83 @@
+# Append orthographic and semantic similarity values to the dataset
+library(stringdist)
+library(rjson)
+library(lsa)
+
+# Load in semantic vectors
+setwd("~/git/sourcemem_models/BaysCataloHussain/data/word2vec")
+
+# Load in word2vec semantic vectors
+word2vec <- fromJSON(file = 'word2vec_filtered.json')
+n_trials <- 10
+
+cosine_distance <- function(theta, phi){
+  distance <- 1 - cos(theta - phi)
+  return(distance)
+}
+
+append_distances <- function(data){
+  distances <- c()
+  for (i in 1:nrow(data)){
+    this_target <- data$target_angle[i]
+    this_intrusions <- data[i, 14:22]
+    this_trial_distances <- cosine_distance(this_target, this_intrusions)
+    distances <- rbind(distances, this_trial_distances)
+  }
+  
+  colnames(distances) <- c("distance_1", "distance_2", "distance_3", "distance_4", 
+                           "distance_5", "distance_6", "distance_7", "distance_8", 
+                           "distance_9")
+  
+  data <- cbind(data, distances)
+  return(data)
+}
+
+append_orthographic_semantic <- function(data){
+  # First, add the spatial distance
+  data <- append_distances(data)
+  
+  # Then, append the orthographic levenshtein distance, and the semantic cosine similarity.
+  orthographic_similarities <- c()
+  semantic_similarities <- c()
+  for(i in 1:nrow(data)){
+    this_word <- data$word[i]
+    this_trial <- data$present_trial[i]
+    this_block <- data$block[i]
+    this_session <- data$session[i]
+    this_participant <- data$participant[i]
+    
+    this_block_data <- data[(data$participant == this_participant) & (data$session == this_session) & (data$block == this_block),]
+    
+    trials <- 1:n_trials
+    non_targets <- trials[trials!=this_trial]
+    
+    this_block_orthographic <- c()
+    this_block_semantic <- c()
+    idx <- 1
+    for (j in non_targets){
+      this_intruding_word <- this_block_data$word[j]
+      
+      # Find the levenshtein distance between the target word and intruding word
+      # Since all stimuli are 4 letters, the maximum distance is 4 (swapping all letters)
+      str_dist = stringdist(this_word, this_intruding_word, method = 'lv')/4
+      this_block_orthographic[idx] <- str_dist
+      semantic_dist = cosine(word2vec[[tolower(this_word)]], word2vec[[tolower(this_intruding_word)]])
+      
+      # Truncate semantic similarity at 0, per Adam's email (alternative is to shift all values by largest minimum value)
+      if (semantic_dist < 0){
+        semantic_dist <- 0
+      }
+      
+      this_block_semantic[idx] <- semantic_dist
+      idx <- idx + 1
+    }
+    orthographic_similarities <- rbind(orthographic_similarities, this_block_orthographic)
+    semantic_similarities <- rbind(semantic_similarities, this_block_semantic)
+  }
+  colnames(orthographic_similarities) <- c("orthographic_1", "orthographic_2", "orthographic_3", "orthographic_4", 
+                                           "orthographic_5", "orthographic_6", "orthographic_7", "orthographic_8", "orthographic_9")
+  colnames(semantic_similarities) <- c("semantic_1", "semantic_2", "semantic_3", "semantic_4", 
+                                       "semantic_5", "semantic_6", "semantic_7", "semantic_8", "semantic_9")
+  data <- cbind(data, orthographic_similarities, semantic_similarities)
+  return(data)
+}
--- analysis/get_online_data.R
+++ analysis/get_online_data.R
@@ -17,8 +17,8 @@ SERVER.MASTER.API.KEY <- "zjFdXfQ64sgAVwQMx84IhzqzUPygpSguUkeLKLqQBIyxo8kP3yphBq
 # whatever is on the server.
 #####
 setwd("~/git/sourcemem_online/analysis")
-library(ggplot2)
 source("access-data.R")
+source("append_distances.R")
 sessions <- 1:10
 
 # Function to compute angular difference
@@ -43,7 +43,9 @@ completed.users <- get.completed.users(SERVER.BASE.URL, SERVER.PORT,
 #                                        SERVER.PORT, SERVER.MASTER.API.KEY)
 
 
-get_session <- function(p,s){
+# This function will arrange the data with no offset in the intrusions (so "intrusions" really are just all the angles in a block, including the target)
+# We need this format for simulation, as intrusions will be indexed by word serial positions.
+get_session_no_offset <- function(p,s){
   
   this.session.data <- get.session.data.by.user.id(SERVER.BASE.URL, p, s,
                                                    SERVER.PORT, SERVER.MASTER.API.KEY)
@@ -53,6 +55,9 @@ get_session <- function(p,s){
                                                                                                          "response_angle","response_error","source_RT", "valid_RT",
                                                                                                          "block", "session"))))
   ## Add a number of empty columns for intrusions that is equivalent to the number of trials per block -1 
+  
+  ## Mark every row with the session number so it is easily identifiable when joined with other sessions
+  data$session <- s
   
   ## Extract presentation data
   for(i in 1:length(this.session.data$present_trials)){
@@ -93,7 +98,120 @@ get_session <- function(p,s){
   for (i in 1:length(words)){
     index <- find_source_index(words[i],this.session.data$recall_trials)
     data$block[i] <- this.session.data$recall_trials[[index]]$block
-    data$session[i] <- this.session.data$session_number
+    data$target_angle[i] <- this.session.data$recall_trials[[index]]$target_angle
+    data$response_angle[i] <- this.session.data$recall_trials[[index]]$hitting_angle
+    data$response_error[i] <- this.session.data$recall_trials[[index]]$angular_error
+    ## Correcting for an error in the old javascript calculation of angular error  
+    if(data$response_error[i] < -pi){
+      data$response_error[i] <- data$response_error[i] + 4*pi 
+    }
+    data$source_RT[i] <- this.session.data$recall_trials[[index]]$response_time
+    data$valid_RT [i] <- (this.session.data$recall_trials[[index]]$num_fast_attempts == 0 &&
+                            this.session.data$recall_trials[[index]]$num_slow_attempts == 0)
+  }
+  
+  data$recog_band <- ifelse(data$recog_rating >= 1 & data$recog_rating <= 3, 'Unrecognized',
+                            ifelse(data$recog_rating >=8 & data$recog_rating <=9, 'Low',
+                                   ifelse(data$recog_rating ==0, 'High','N/A')))
+  
+  # Convert the indexing to start at 1
+  data$block <- data$block + 1
+  data$present_trial <- data$present_trial + 1
+  
+  # Assemble a list of intrusions for each trial, and append this to the data structure
+  num_intrusions <- 10 # This is n-1 where n is number of trials per block
+  
+  intrusion_labels <- vector()
+  for (i in 1:num_intrusions){
+    intrusion_labels[i] <- sprintf('angle_%i',i)
+  }
+  
+  intrusion_offset_labels <- vector()
+  for (i in 1:num_intrusions){
+    intrusion_offset_labels[i] <- sprintf('offset%i',i)
+  }
+  
+  intrusions <- data.frame(matrix(ncol=10,nrow=125))
+  colnames(intrusions) <- intrusion_labels
+  
+  intrusion_offsets <- data.frame(matrix(ncol=10,nrow=125))
+  colnames(intrusion_offsets) <- intrusion_offset_labels
+  
+  intrusion_lags <- data.frame(matrix(ncol=10,nrow=125))
+  colnames(intrusion_offsets) <- intrusion_offset_labels
+  
+  for (i in 1:nrow(data)){
+    this_block_data <- data[data$block == data$block[i],]
+    # The possible intrusions are items in the same block as this trial, but do not have the same stimulus word
+    this_block_intrusions <- this_block_data$target_angle
+    this_intrusion_offsets <- angle_diff(data$target_angle[i], this_block_intrusions)
+    if (data$block[i] == -1){
+      this_block_intrusions <- c(this_block_intrusions, NA, NA, NA, NA, NA)
+      this_intrusion_offsets <- c(this_intrusion_offsets, NA, NA, NA, NA, NA) # Filling up with NAs for practice block
+    }
+    intrusions[i,] <- this_block_intrusions
+    intrusion_offsets[i,] <- this_intrusion_offsets
+  }
+  data <- cbind(data,intrusions, intrusion_offsets)
+  return(data)
+}
+
+
+# This function gets the data with the target removed from the list of angles, so it's actually a list of intrusions
+
+get_session_offset <- function(p,s){
+  
+  this.session.data <- get.session.data.by.user.id(SERVER.BASE.URL, p, s,
+                                                   SERVER.PORT, SERVER.MASTER.API.KEY)
+  ## Extract the required information for each stimuli across the trial types.
+  data <- data.frame(matrix(ncol=12,nrow=length(this.session.data$present_trials), dimnames=list(NULL, c("word", "is_sequential", "present_trial",
+                                                                                                         "recog_rating","recog_RT","target_angle",
+                                                                                                         "response_angle","response_error","source_RT", "valid_RT",
+                                                                                                         "block", "session"))))
+  ## Add a number of empty columns for intrusions that is equivalent to the number of trials per block -1 
+  
+  ## Mark every row with the session number so it is easily identifiable when joined with other sessions
+  data$session <- s
+  
+  ## Extract presentation data
+  for(i in 1:length(this.session.data$present_trials)){
+    data$word[i] <- this.session.data$present_trials[[i]]$target_word
+    data$is_sequential[i] <- this.session.data$present_trials[[i]]$presentation_sequential
+    data$present_trial[i] <- this.session.data$present_trials[[i]]$trial
+  }
+  
+  ## Function to sort through the scrambled blocks and find the index needed for extraction
+  find_recog_index <- function(x, list){
+    for (i in 1:length(list)){
+      if (list[[i]]$stimulus == x){
+        return(i)
+      } 
+    }
+  }
+  
+  ## The stimulus word is called "target_angle" for the recall trials. Could change how it is named in the database, 
+  ## but easier for now to just use the different name here
+  find_source_index <- function(x, list){
+    for (i in 1:length(list)){
+      if (list[[i]]$target_word == x){
+        return(i)
+      } 
+    }
+  }
+  
+  ## Extract recognition data
+  words <- unique(data$word)
+  
+  for (i in 1:length(words)){
+    index <- find_recog_index(words[i],this.session.data$confidence_trials)
+    data$recog_rating[i] <- this.session.data$confidence_trials[[index]]$response
+    data$recog_RT[i] <- this.session.data$confidence_trials[[index]]$rt
+  } 
+  
+  ## Extract source recall data
+  for (i in 1:length(words)){
+    index <- find_source_index(words[i],this.session.data$recall_trials)
+    data$block[i] <- this.session.data$recall_trials[[index]]$block
     data$target_angle[i] <- this.session.data$recall_trials[[index]]$target_angle
     data$response_angle[i] <- this.session.data$recall_trials[[index]]$hitting_angle
     data$response_error[i] <- this.session.data$recall_trials[[index]]$angular_error
@@ -161,119 +279,16 @@ get_session <- function(p,s){
   return(data)
 }
 
-
-# Plot a particular session for a participant
-plot_session <- function(p,s){
-  
-  data <- get_session(p,s)
-  
-  high_error <- ggplot(data = data[data$recog_band == 'High',], aes(x = response_error)) +
-    geom_histogram(bins = 50) +
-    labs(title ="High Recognition Ratings", x = "Response Error (radians)", y = "Frequency") +
-    theme_classic()
-  
-  low_error <- ggplot(data = data[data$recog_band == 'Low',], aes(x = response_error)) +
-    geom_histogram(bins = 50) +
-    labs(title ="Low Recognition Ratings", x = "Response Error (radians)", y = "Frequency") +
-    theme_classic()
-  
-  unrecog_error <- ggplot(data = data[data$recog_band == 'Unrecognized',], aes(x = response_error)) +
-    geom_histogram(bins = 50) +
-    labs(title ="Unrecognized Recognition Ratings", x = "Response Error (radians)", y = "Frequency") +
-    theme_classic()
-  
-  error <- ggplot(data = data, aes(x = response_error)) +
-    geom_histogram(bins = 50) +
-    labs(title ="All Recognition Ratings", x = "Response Error (radians)", y = "Frequency") +
-    theme_classic()
-  
-  rt <- ggplot(data = data, aes(x = source_RT)) +
-    geom_histogram(bins = 50) +
-    labs(title ="All Recognition Ratings", x = "Response Time (ms)", y = "Frequency") +
-    theme_classic()
-  
-  #print(high_error)
-  print(error)
-  print(rt)
-  return(data)
-}  
-
-## Plot histograms of response error and response times for all sessions for this participant
-
-plot_participant <- function(p){
-  this.user.data = data.frame()
-  for(s in 1:length(sessions)){
-    
-    this.session.data <- get_session(p,s)
-    # Bind sessions together
-    
-    this.user.data <- rbind(this.user.data, this.session.data)
-    
-    # Filter out invalid RTs
-    # this.user.data <- this.user.data[this.user.data$valid_RT == TRUE,]
-  }
-  
-  
-  
-  # high_error <- ggplot(data = this.user.data[this.user.data$recog_band == 'High',], aes(x = response_error)) +
-  #   geom_histogram(bins = 50) +
-  #   labs(title ="High Recognition Ratings", x = "Response Error (radians)", y = "Frequency") +
-  #   theme_classic()
-  # 
-  # low_error <- ggplot(data = this.user.data[this.user.data$recog_band == 'Low',], aes(x = response_error)) +
-  #   geom_histogram(bins = 50) +
-  #   labs(title ="Low Recognition Ratings", x = "Response Error (radians)", y = "Frequency") +
-  #   theme_classic()
-  # 
-  # unrecog_error <- ggplot(data = this.user.data[this.user.data$recog_band == 'Unrecognized',], aes(x = response_error)) +
-  #   geom_histogram(bins = 50) +
-  #   labs(title ="Unrecognized Recognition Ratings", x = "Response Error (radians)", y = "Frequency") +
-  #   theme_classic()
-  
-  if (this.user.data$is_sequential){
-    cond = "Sequential"
-  } else {
-    cond = "Simultaneous"
-  }
-  
-  title <- sprintf("Participant ID: %.0f \nCondition: %s \nResponse Times", p, cond)
-  
-  rt <- ggplot(data = this.user.data[this.user.data$recog_band != 'Unrecognized',], aes(x = source_RT)) +
-    geom_histogram(bins = 50) +
-    labs(title = title, x = "Response Time (ms)", y = "Frequency") +
-    theme_classic()
-  
-  title <- sprintf("Participant ID: %.0f \nCondition: %s \nResponse Error", p, cond)
-  
-  error <- ggplot(data = this.user.data[this.user.data$recog_band != 'Unrecognized',], aes(x = response_error)) +
-    geom_histogram(bins = 50) +
-    labs(title = title, x = "Response Error (radians)", y = "Frequency") +
-    theme_classic()
-  
-  print(error)
-  print(rt)
-  
-  # Save Plots
-  
-  filename = sprintf('Subject_%.0f_Error.png', p)
-  ggsave(filename, plot = error)
-  
-  filename = sprintf('Subject_%.0f_RT.png', p)
-  ggsave(filename, plot = rt)
-  
-  return(this.user.data)
-}
-
 ## Save all the data as one csv
 
-save.all.data <- function(filename){
+save.data.no.offset <- function(){
   all.data = data.frame()
   # Participant Loop
   for(h in 1:length(completed.users)){
     this.user.data = data.frame()
     # Session Loop
     for(j in 1:length(sessions)){
-      data <- get_session(completed.users[[h]],j)
+      data <- get_session_no_offset(completed.users[[h]],j)
       # Bind sessions together
       
       this.user.data <- rbind(this.user.data, data)
@@ -291,16 +306,50 @@ save.all.data <- function(filename){
   all.data[all.data$recog_rating == 0,]$recog_rating <- 6
   
   # Save data as .csv file
-  write.csv(all.data, filename, row.names = FALSE)
-  
-  ## Save all users data
-  # for(i in 1:length(completed.users)){
-  #   this.user.data <- get.last.experiment.data.by.user.id(SERVER.BASE.URL, completed.users[[i]],
-  #                                                         SERVER.PORT, SERVER.MASTER.API.KEY)
-  #   filename = sprintf('Subject%s.RData', i)
-  #   save(this.user.data, file = filename)
-  # }
+  write.csv(all.data,"~/git/sourcemem_online/analysis/smalln_no_offset.csv", row.names = FALSE)
   return(all.data)
 }
 
+save.data <- function(){
+  all.data = data.frame()
+  # Participant Loop
+  for(h in 1:length(completed.users)){
+    this.user.data = data.frame()
+    # Session Loop
+    for(j in 1:length(sessions)){
+      data <- get_session_offset(completed.users[[h]],j)
+      # Bind sessions together
+      
+      this.user.data <- rbind(this.user.data, data)
+      # Filter out invalid RTs
+      #this.user.data <- this.user.data[this.user.data$valid_RT == TRUE,]
+      
+    }
+    this.user.data$participant <- h
+    all.data <- rbind(all.data, this.user.data)
+  }
+  
+  # Recode the confidence ratings to be between 1 and 6
+  all.data[all.data$recog_rating == 8,]$recog_rating <- 4
+  all.data[all.data$recog_rating == 9,]$recog_rating <- 5
+  all.data[all.data$recog_rating == 0,]$recog_rating <- 6
+  
+  all.data <- all.data[all.data$block != 0,]
+  all.data <- append_orthographic_semantic(all.data)
+  # Save data as .csv file
+  write.csv(all.data,"~/git/sourcemem_online/analysis/smalln_offset.csv", row.names = FALSE)
+  return(all.data)
+}
+
+## Save all users data
+save.raw.data <- function(){
+  for(i in 1:length(completed.users)){
+    for (j in 1:10){
+      this.session.data <- get.session.data.by.user.id(SERVER.BASE.URL, i, j,
+                                                       SERVER.PORT, SERVER.MASTER.API.KEY)
+      filename = sprintf('Subject%s_Session%s.RData', i, j)
+      save(this.session.data, file = filename)
+    }
+  }
+}
 
