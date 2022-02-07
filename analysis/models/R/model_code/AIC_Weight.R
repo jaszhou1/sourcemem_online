cdad538eec0cfe9058c75142bb54a078cec5cf65
Jason Zhou <jasonz1@student.unimelb.edu.au>
2022-02-08 04:02:03 +1100

    Calculate AIC weights for Experiment 2

--- analysis/models/R/model_code/AIC_Weight.R
+++ analysis/models/R/model_code/AIC_Weight.R
@@ -1,13 +1,21 @@
-setwd("~/git/sourcemem_online/analysis/models/R/model_code")
-AIC <- read.csv('Exp1_AIC.csv')
-for(i in 1:nrow(AIC)){
-  AIC[i, 6] <- min(AIC[i, 1:5])
+
+AIC_weight <- function(AIC){
+  ncol <- ncol(AIC)
+  for(i in 1:nrow(AIC)){
+    AIC[i, ncol+1] <- min(AIC[i, 1:ncol])
+  }
+  
+  delta_AIC <- AIC[,1:ncol] - AIC[,ncol+1]
+  relative_AIC <- exp( -0.5 * delta_AIC)
+  relative_AIC[,ncol+1] <- rowSums(relative_AIC)
+  
+  AIC_weight <- relative_AIC[,1:ncol]/relative_AIC[,ncol+1]
+  
+  write.csv(AIC_weight, file = 'AIC_Weight.csv')
 }
 
-delta_AIC <- AIC[,1:5] - AIC[,6]
-relative_AIC <- exp( -0.5 * delta_AIC)
-relative_AIC[,6] <- rowSums(relative_AIC)
+setwd("~/git/sourcemem_online/analysis/models/R/experiment_1/output")
+exp1 <- read.csv('Exp1_AIC.csv')
 
-AIC_weight <- relative_AIC[,1:5]/relative_AIC[,6]
-
-write.csv(AIC_weight, file = 'AIC_Weight.csv')
+setwd("~/git/sourcemem_online/analysis/models/R/experiment_2/output")
+exp2 <- read.csv('Exp2_AIC.csv')
