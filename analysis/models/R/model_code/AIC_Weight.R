5de7718b9e6f1b5fef599ad12b85c24fb26bf1b9
Jason Zhou <jasonz1@student.unimelb.edu.au>
2022-02-23 17:05:29 +1100

    Raw AIC and AIC weight in output table
    Want to report raw AIC values as well as AIC weights.

--- analysis/models/R/model_code/AIC_Weight.R
+++ analysis/models/R/model_code/AIC_Weight.R
@@ -10,8 +10,17 @@ AIC_weight <- function(AIC, filename){
   relative_AIC[,ncol+1] <- rowSums(relative_AIC)
   
   AIC_weight <- relative_AIC[,1:ncol]/relative_AIC[,ncol+1]
+  AIC[,ncol+1] <- NULL
   
-  write.csv(AIC_weight, file = filename)
+  # Interleave raw AIC values with AIC weights
+  # AIC <- cbind(AIC, AIC_weight)
+  # idx <- rep(1:ncol, each = 2) + (0:1) * ncol <- a cool solution!
+  # AIC <- AIC[idx]
+  
+  # Stack raw AIC and AIC weights
+  AIC <- rbind(AIC, AIC_weight)
+  
+  write.csv(AIC, file = filename)
 }
 
 setwd("~/git/sourcemem_online/analysis/models/R/experiment_1/output")
@@ -21,4 +30,4 @@ setwd("~/git/sourcemem_online/analysis/models/R/experiment_2/output")
 exp2 <- read.csv('Exp2_AIC_trimmed.csv')
 
 setwd("~/git/sourcemem_online/analysis/models/MATLAB/experiment_2")
-exp2_diff <- read.csv("~/git/sourcemem_online/analysis/models/MATLAB/experiment_2/exp2_AICs.csv")
+exp2_diff <- read.csv("~/git/sourcemem_online/analysis/models/MATLAB/experiment_2/AIC_v3.csv")
