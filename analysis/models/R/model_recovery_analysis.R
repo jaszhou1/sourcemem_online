4f0d88874727ad38f4b71673b8f40fa995d28103
Jason Zhou <jasonz1@student.unimelb.edu.au>
2022-03-05 15:57:30 +1100

    5 runs of each model in model recovery

--- analysis/models/R/model_recovery_analysis.R
+++ analysis/models/R/model_recovery_analysis.R
@@ -2,21 +2,27 @@
 # and see how many times the correct model is recovered.
 
 setwd("~/git/sourcemem_online/analysis/models/R/model_recovery_crossfits")
-load("2022-02-24_P1_crossfit.RData")
+load("2022-03-03_P1_crossfit.RData")
 P1 <- crossfit
 
-load("2022-02-24_P2_crossfit.RData")
+load("2022-03-03_P2_crossfit.RData")
 P2 <- crossfit
 
-load("2022-02-24_P3_crossfit.RData")
+load("2022-03-03_P3_crossfit.RData")
 P3 <- crossfit
 
-load("2022-02-24_P4_crossfit.RData")
+load("2022-03-03_P4_crossfit.RData")
 P4 <- crossfit
 
-load("2022-02-24_P5_crossfit.RData")
+load("2022-03-02_P5_crossfit.RData")
 P5 <- crossfit
 crossfits <- rbind(P1,P2,P3,P4,P5)
+
+# ## Temporary chunk to flag each run number
+# run_idx <- rep(1:3, each = 4)
+# run_idx <- rep(run_idx, 20)
+# crossfits$n_run <- run_idx
+# ## End temporary part
 
 PARTICIPANTS <- unique(crossfits$participant)
 MODELS <- unique(crossfits$gen_model)
@@ -24,12 +30,14 @@ MODELS <- unique(crossfits$gen_model)
 for(i in 1:nrow(crossfits)){
   this_participant <- crossfits[i, 1]
   this_gen_model <- crossfits[i, 2]
+  this_run <- crossfits[i, 5]
   this_crossfit <- crossfits[(crossfits$participant == this_participant) & 
-                               (crossfits$gen_model == this_gen_model),]
-  crossfits[i, 5] <- crossfits[i, 4] - min(this_crossfit$aic)
+                               (crossfits$gen_model == this_gen_model) &
+                               crossfits$run == this_run,]
+  crossfits[i, 6] <- crossfits[i, 4] - min(this_crossfit$aic)
 }
 
-colnames(crossfits)[5] <- 'delta_AIC'
+colnames(crossfits)[6] <- 'delta_AIC'
 
 model_recovery_prop <- function(this_model){
   # Find the number of times the generating model matches the fitting model
