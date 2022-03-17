950b12bf6e16c921dbdb295a091adf25910a301f
jaszhou1 <jasonz1@student.unimelb.edu.au>
2022-03-17 12:23:12 +1100

    diffusion fits manually tweaked starting param

--- analysis/models/MATLAB/fit_spatiotemporal_model.m
+++ analysis/models/MATLAB/fit_spatiotemporal_model.m
@@ -6,23 +6,23 @@ if nargin < 2
     badix = 5;
 end
 
-v1_targ = normrnd(4, 0.5);
+v1_targ = 4.5;
 v2_targ = 0;
-v1_int = normrnd(1.3, 0.5);
+v1_int = 0.05;
 v2_int = 0;
-eta_targ = normrnd(0.3, 0.1);
-eta_int = normrnd(0.03, 0.1);
-a_targ = normrnd(3, 0.5);
+eta_targ = 0.05;
+eta_int = 0.05;
+a_targ = normrnd(1.5, 0.1);
 % a_int = normrnd(1.2, 0.1);
-a_guess = normrnd(1.8, 0.2);
+a_guess = normrnd(0.6, 0.05);
 gamma = abs(normrnd(0.1, 0.01));
-beta = abs(normrnd(0.1, 0.05));
-kappa = normrnd(0.8, 0.05);
-lambda_b = abs(normrnd(0.2, 0.2));
-lambda_f = abs(normrnd(1, 0.2));
-zeta = normrnd(0.9, 0.01);
-rho = normrnd(0.75, 0.1);
-Ter = normrnd(0.05, 0.05);
+beta = abs(normrnd(0.7, 0.05));
+kappa = normrnd(0.5, 0.05);
+lambda_b = abs(normrnd(0.4, 0.1));
+lambda_f = abs(normrnd(0.8, 0.1));
+zeta = normrnd(0.01, 0.001);
+rho = normrnd(0.9, 0.05);
+Ter = normrnd(0.1, 0.05);
 st = 0;
 
 
