1fe3423b797d2507bb190a17470bef7e6e48d50b
jason zhou <45774327+jaszhou1@users.noreply.github.com>
2022-11-20 12:58:52 +1100

    AIC weight
    Calculate AIC weights from raw AIC values

---
+++ analysis/models/MATLAB/revision/AIC_Weight.R
@@ -0,0 +1,30 @@
+
+AIC_weight <- function(AIC, filename){
+  ncol <- ncol(AIC)
+  for(i in 1:nrow(AIC)){
+    AIC[i, ncol+1] <- min(AIC[i, 1:ncol])
+  }
+  
+  delta_AIC <- AIC[,1:ncol] - AIC[,ncol+1]
+  relative_AIC <- exp(-0.5 * delta_AIC)
+  relative_AIC[,ncol+1] <- rowSums(relative_AIC)
+  
+  AIC_weight <- relative_AIC[,1:ncol]/relative_AIC[,ncol+1]
+  AIC[,ncol+1] <- NULL
+  
+  # Interleave raw AIC values with AIC weights
+  # AIC <- cbind(AIC, AIC_weight)
+  # idx <- rep(1:ncol, each = 2) + (0:1) * ncol 
+  # AIC <- AIC[idx]
+  # 
+  # Stack raw AIC and AIC weights
+  AIC <- rbind(AIC, AIC_weight)
+  
+  write.csv(AIC, file = filename)
+}
+
+# setwd("~/git/sourcemem_online/analysis/models/R/experiment_1/output")
+# exp1 <- read.csv('Exp1_AIC.csv')
+
+
+exp2 <- read.csv('exp2_AIC.csv')
